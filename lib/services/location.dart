5885290c36e348512071b3e357180afce8c27901
LVTDEL <votheluc01@gmail.com>
2025-01-10 21:06:36 +0700

    Done lab 9

---
+++ lib/services/location.dart
@@ -0,0 +1,19 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  double latitude = 0;
+  double longitude = 0;
+
+  Future<void> getCurrentLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          locationSettings:
+              const LocationSettings(accuracy: LocationAccuracy.low));
+
+      latitude = position.latitude;
+      longitude = position.longitude;
+    } catch (e) {
+      print(e);
+    }
+  }
+}
